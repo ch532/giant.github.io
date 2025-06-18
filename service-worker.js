1e6eb135f818e4686a2600ea285b4dfbe32adda3
ch532 <81508031+ch532@users.noreply.github.com>
2025-06-18 08:07:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'connectgold-cache-v2.4.0'; // 🔁 Bump this when updating
+const CACHE_NAME = 'connectgold-cache-v2.6.0'; // 🔁 Bump this when updating
 const urlsToCache = [
   '/manifest.json',
   '/connect gold (1).png',
