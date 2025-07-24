52a2cc4c4ca25207ecf38cb334648a9b3dc725fd
ch532 <trustconnect713@gmail.com>
2025-07-24 23:36:35 +0100

    Update MainActivity.java

--- app/src/main/java/co.median.android.aaoraq/MainActivity.java
+++ app/src/main/java/co.median.android.aaoraq/MainActivity.java
@@ -1,31 +1,30 @@
-package co.median.android.aaoraq;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import com.appodeal.ads.Appodeal;
+import com.huawei.openalliance.ad.jsb.PPSJsBridge;
+import com.huawei.openalliance.ad.jsb.inner.JsbConfig;
 
 public class MainActivity extends Activity {
 
-    private WebView webView;
+    private WebView mWebview;
+    private PPSJsBridge bridge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Make sure you have this layout with a WebView
+        setContentView(R.layout.activity_main);
 
-        // 1. Initialize Appodeal
-        Appodeal.initialize(this, "923bc30ebaca5186de21bbebb9612173a8759ba61bbb4ed0",
-                Appodeal.BANNER | Appodeal.INTERSTITIAL | Appodeal.REWARDED_VIDEO);
+        mWebview = findViewById(R.id.webView);
+        mWebview.getSettings().setJavaScriptEnabled(true);
+        mWebview.getSettings().setDomStorageEnabled(true);
 
-        // 2. Setup WebView
-        webView = findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.addJavascriptInterface(new AppodealJSInterface(this), "AndroidAdInterface");
+        PPSJsBridge.init(new JsbConfig.Builder()
+                .enableUserInfo(true)
+                .enableLog(true)
+                .build());
 
-        // 3. Load your hosted page
-        webView.loadUrl("https://connectgold.sbs/index.html");
+        bridge = new PPSJsBridge(mWebview);
+
+        mWebview.loadUrl("file:///android_asset/index.html"); // or remote URL
     }
 }
