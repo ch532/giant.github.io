be8aa788720f50944ae77fe82c56c5d255f3c8a9
ch532 <trustconnect713@gmail.com>
2025-07-28 04:53:05 +0100

    Update Mainactivity.java

--- Mainactivity.java
+++ Mainactivity.java
@@ -3,14 +3,15 @@ package co.median.android.aaoraq;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.JavascriptInterface;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.startapp.sdk.adsbase.StartAppAd;
 import com.startapp.sdk.adsbase.StartAppSDK;
-
 import com.unity3d.ads.IUnityAdsListener;
 import com.unity3d.ads.UnityAds;
 
@@ -31,39 +32,43 @@ public class MainActivity extends AppCompatActivity {
 
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
+        webSettings.setMediaPlaybackRequiresUserGesture(false); // for Unity video
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("file:///android_asset/index.html"); // or your main HTML page
+        myWebView.setWebChromeClient(new WebChromeClient()); // optional
+
+        myWebView.loadUrl("file:///android_asset/index.html");
 
         // Attach JS interface
         myWebView.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
 
-        // Initialize Start.io
+        // Start.io initialization
         StartAppSDK.init(this, "205787982", true);
-        StartAppAd.disableSplash(); // Optional: remove Start.io splash ad
+        StartAppAd.disableSplash();
 
-        // Initialize Unity Ads
+        // Unity initialization
         UnityAds.initialize(this, UNITY_GAME_ID, TEST_MODE);
         UnityAds.setListener(new UnityAdsListener());
 
-        // Auto-show Start.io interstitial after delay
-        myWebView.postDelayed(() -> {
-            StartAppAd.showAd(MainActivity.this);
-        }, 7000); // 7 seconds after launch
+        // Auto-show Start.io Interstitial
+        myWebView.postDelayed(() -> StartAppAd.showAd(MainActivity.this), 7000);
 
         // Auto-show Unity Interstitial
         myWebView.postDelayed(() -> {
             if (UnityAds.isReady(UNITY_INTERSTITIAL)) {
                 UnityAds.show(MainActivity.this, UNITY_INTERSTITIAL);
+            } else {
+                Log.d("UnityAd", "Unity Interstitial not ready");
             }
-        }, 15000); // 15 seconds after launch
+        }, 15000);
 
         // Auto-show Unity Rewarded
         myWebView.postDelayed(() -> {
             if (UnityAds.isReady(UNITY_REWARDED)) {
                 UnityAds.show(MainActivity.this, UNITY_REWARDED);
+            } else {
+                Log.d("UnityAd", "Unity Rewarded not ready");
             }
-        }, 30000); // 30 seconds after launch
+        }, 30000);
     }
 
     public class MyJavaScriptInterface {
@@ -95,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onUnityAdsFinish(String placementId, UnityAds.FinishState result) {}
         @Override
-        public void onUnityAdsError(UnityAds.UnityAdsError error, String message) {}
+        public void onUnityAdsError(UnityAds.UnityAdsError error, String message) {
+            Log.e("UnityAdsError", message);
+        }
     }
 }
