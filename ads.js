318f9bae02070b4522e991a63af1042652408b3a
ch532 <trustconnect713@gmail.com>
2025-07-31 06:18:36 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,31 +1,14 @@
-let adDisplayContainer;
-let adsLoader;
-let adsManager;
-let videoContent;
+let adsLoader, adsManager;
+let adDisplayContainer, videoContent;
 
-function initAdDisplayContainer() {
+const vastTag = "https://essentialmost.com/d/mmF/z.d/GpNpvxZnGTUU/Weomv9RuQZpUNlkk/PATxY/1tNkTWAsxbNuDUcjtQN/jcUo1ZMlDnE/0WOQCTZLsoa/Wl1-pTdrDt0/xu";
+
+function initAd() {
+  videoContent = document.getElementById('content_video');
   adDisplayContainer = new google.ima.AdDisplayContainer(
-    document.getElementById('adContainer'),
-    videoContent
+    document.getElementById('ad_container'), videoContent
   );
-}
-
-function requestAds() {
-  const adsRequest = new google.ima.AdsRequest();
-  adsRequest.adTagUrl = "https://essentialmost.com/d/mmF/z.d/GpNpvxZnGTUU/Weomv9RuQZpUNlkk/PATxY/1tNkTWAsxbNuDUcjtQN/jcUo1ZMlDnE/0WOQCTZLsoa/Wl1-pTdrDt0/xu";
-
-  adsRequest.linearAdSlotWidth = 640;
-  adsRequest.linearAdSlotHeight = 360;
-
-  adsRequest.nonLinearAdSlotWidth = 640;
-  adsRequest.nonLinearAdSlotHeight = 150;
-
-  adsLoader.requestAds(adsRequest);
-}
-
-function setUpIMA() {
-  videoContent = document.getElementById('contentElement');
-  initAdDisplayContainer();
+  adDisplayContainer.initialize();
 
   adsLoader = new google.ima.AdsLoader(adDisplayContainer);
 
@@ -41,38 +24,40 @@ function setUpIMA() {
     false
   );
 
-  requestAds();
+  const adsRequest = new google.ima.AdsRequest();
+  adsRequest.adTagUrl = vastTag;
+  adsRequest.linearAdSlotWidth = 640;
+  adsRequest.linearAdSlotHeight = 360;
+  adsRequest.nonLinearAdSlotWidth = 640;
+  adsRequest.nonLinearAdSlotHeight = 150;
+
+  adsLoader.requestAds(adsRequest);
 }
 
 function onAdsManagerLoaded(adsManagerLoadedEvent) {
   adsManager = adsManagerLoadedEvent.getAdsManager(videoContent);
 
-  adsManager.addEventListener(
-    google.ima.AdErrorEvent.Type.AD_ERROR,
-    onAdError
-  );
-
-  adsManager.addEventListener(
-    google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED,
-    () => videoContent.play()
-  );
+  adsManager.addEventListener(google.ima.AdErrorEvent.Type.AD_ERROR, onAdError);
+  adsManager.addEventListener(google.ima.AdEvent.Type.CONTENT_RESUME_REQUESTED, () => {
+    videoContent.play();
+  });
 
   try {
     adsManager.init(640, 360, google.ima.ViewMode.NORMAL);
     adsManager.start();
-  } catch (adError) {
-    console.log("AdsManager could not be started: " + adError);
+  } catch (e) {
+    console.log('AdsManager could not be started', e);
     videoContent.play();
   }
 }
 
 function onAdError(adErrorEvent) {
-  console.log("Ad error: ", adErrorEvent.getError());
+  console.log('Ad Error:', adErrorEvent.getError());
+  if (adsManager) adsManager.destroy();
   videoContent.play();
 }
 
-document.getElementById('playButton').addEventListener('click', () => {
-  videoContent.load();
-  adDisplayContainer.initialize();
-  setUpIMA();
+document.getElementById('play_button').addEventListener('click', function () {
+  this.style.display = 'none';
+  initAd();
 });
