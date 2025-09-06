f581e0aeda13372830de6a2eb111267d0611b18a
ch532 <trustconnect713@gmail.com>
2025-09-06 07:01:11 +0100

    Update showAdButton.jsx

--- showAdButton.jsx
+++ showAdButton.jsx
@@ -1,19 +1,20 @@
-import { useCallback, ReactElement } from 'react'
-import { useAdsgram } from "./hooks/useAdsgram.ts";
-
+import { useCallback } from 'react'
+import { useAdsgram } from "../hooks/useAdsgram"
 
 export function ShowAdButton() {
   const onReward = useCallback(() => {
     alert('Reward');
   }, []);
+
   const onError = useCallback((result) => {
     alert(JSON.stringify(result, null, 4));
   }, []);
 
-  /**
-   * insert your-block-id
-   */
-  const showAd = useAdsgram({ blockId: "your-block-id", onReward, onError });
+  const showAd = useAdsgram({
+    blockId: "14637", // ✅ replace with your Adsgram blockId
+    onReward,
+    onError
+  });
 
   return (
     <button onClick={showAd}>Show Ad</button>
