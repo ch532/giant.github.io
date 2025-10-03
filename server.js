3bc4560720c6bf81febd133c6899b93e124b2a18
ch532 <trustconnect713@gmail.com>
2025-10-03 12:04:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Allow requests only from your website
 app.use(cors({
